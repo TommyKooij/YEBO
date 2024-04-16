a359b36e00b100f4605251413e7764c5f42f02fc
TommyKooij <74959713+TommyKooij@users.noreply.github.com>
2024-04-16 12:29:48 +0200

    Fixed Goal text and Topic cards & added image for testimonial

--- packages/mars-theme/src/pages/Home.jsx
+++ packages/mars-theme/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { styled } from "frontity";
 import savanne from "../img/IMG_4826.jpg";
+import people from "../img/people.png";
+import school from "../img/school.png";
+import support from "../img/support.png";
 
 export function HomePage(props) {
   return (
@@ -9,17 +12,40 @@ export function HomePage(props) {
       <Title>Een betere toekomst door onderwijs</Title>
       <Button>Help kinderen in Zuid-Afrika</Button>
       <Goal>
-        <h2>BIJDRAGEN AAN EEN BETERE TOEKOMST DOOR ONDERWIJS?</h2>
-        <h4>
-          Een vervolgopleiding is de sleutel tot een betere toekomst. Onze
-          stichting helpt jongeren uit kansarme gezinnen in Schoemansdal
-          (Zuid-Afrika) door het aanbieden van een studiefonds.
-        </h4>
+        <GoalContent>
+          <h2>BIJDRAGEN AAN EEN BETERE TOEKOMST DOOR ONDERWIJS?</h2>
+          <h3>
+            Een vervolgopleiding is de sleutel tot een betere toekomst. Onze
+            stichting helpt jongeren uit kansarme gezinnen in Schoemansdal,
+            Zuid-Afrika, door het aanbieden van een studiefonds.
+          </h3>
+        </GoalContent>
       </Goal>
       <Topics>
-        <TopicCard>#1</TopicCard>
-        <TopicCard>#2</TopicCard>
-        <TopicCard>#3</TopicCard>
+        <TopicCard>
+          <h2>Wie zijn wij?</h2>
+          <Icon alt="peope" src={people} />
+          <h3>
+            YEBO is een non-profit organisatie die studiefondsen uitleent aan
+            kansarme kinderen in Schoemansdal, Zuid-Afrika.
+          </h3>
+        </TopicCard>
+        <TopicCard>
+          <h2>Wat doen wij?</h2>
+          <Icon alt="school" src={school} />
+          <h3>
+            Samen met onze donateurs proberen wij kinderen in Schoemansdal een
+            kans op onderwijs te geven.
+          </h3>
+        </TopicCard>
+        <TopicCard>
+          <h2>Steun ons!</h2>
+          <Icon alt="support" src={support} />
+          <h3>
+            Steun ons en help kinderen in Zuid-Afrika met het krijgen van een
+            studiefonds, omdat elk kind recht heeft op goede educatie.
+          </h3>
+        </TopicCard>
       </Topics>
       <Testimonial>lol</Testimonial>
     </Container>
@@ -62,14 +88,20 @@ const Button = styled.button`
 `;
 
 const Goal = styled.div`
-  width: 800px;
   max-width: 100%;
-  padding-top: 12px;
-  padding-bottom: 24px;
+  padding-top: 16px;
+  padding-bottom: 32px;
   box-sizing: border-box;
-  justify-content: space-around;
-  text-align: center;
+  align-items: center;
   color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const GoalContent = styled.div`
+  width: 800px;
+  text-align: center;
 `;
 
 const Topics = styled.div`
@@ -78,14 +110,22 @@ const Topics = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  border-top: 1px solid black;
 `;
 
 const TopicCard = styled.div`
   width: 33.33%;
   height: 300px;
+  padding: 8px 16px;
   text-align: center;
-  background-color: red;
-  border-right: 1px solid black;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Icon = styled.img`
+  height: 40%;
+  width: 33%;
 `;
 
 const Testimonial = styled.div`
